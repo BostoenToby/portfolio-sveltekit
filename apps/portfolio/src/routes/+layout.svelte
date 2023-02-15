f66cab400ce0fa05a44e5a3940d9e5013de8a7ed
BostoenToby <toby.bostoen@student.howest.be>
2023-02-15 22:05:54 +0100

    📱 Home page and layout created

---
+++ apps/portfolio/src/routes/+layout.svelte
@@ -0,0 +1,178 @@
+<script lang="ts">
+	import '@fontsource/montserrat'
+	import { Sun, Home, Cpu, Contact, Download } from 'lucide-svelte'
+</script>
+
+<style scoped>
+	:root {
+		--green: #339989;
+		--gray: #e2e8f0;
+	}
+
+	:global(body, html) {
+		height: 100%;
+		margin: 0;
+		font-family: 'Montserrat', sans-serif;
+	}
+
+	a {
+		text-decoration: none;
+		color: black;
+	}
+
+	:global(h1, h2, h3, h4, h5, h6, p) {
+		margin: 0;
+		padding: 0;
+	}
+
+	.page {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+	}
+
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem 1.5rem;
+	}
+
+	.title {
+		font-size: 1rem;
+		color: var(--green);
+		padding-bottom: 6px;
+		border-bottom: 3px solid var(--green);
+		cursor: pointer;
+	}
+
+	.nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		gap: 4rem;
+	}
+
+	.container-right {
+		display: flex;
+		align-items: center;
+		gap: 2rem;
+	}
+
+	.sun-container {
+		border: none;
+		background-color: inherit;
+		color: var(--green);
+		cursor: pointer;
+	}
+
+	.contact {
+		font-size: 1rem;
+		padding: 1rem;
+		background-color: var(--gray);
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+	}
+
+	.contact:hover {
+		box-shadow: 2px 2px 10px var(--gray);
+	}
+
+	.container {
+		display: flex;
+		flex: 1 1 auto;
+	}
+
+	.footer {
+		position: sticky;
+		bottom: 0;
+		width: 100%;
+		height: 80px;
+		border-top: solid 1px var(--gray);
+		background-color: white;
+	}
+
+	.footer-container {
+		height: 100%;
+		margin: auto;
+		display: flex;
+		width: 80%;
+		justify-content: space-between;
+	}
+
+	.navigation-bottom {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 0.5rem;
+		padding: 8px 0;
+	}
+
+	.navigation-top:hover {
+		color: var(--green);
+		padding-bottom: 4px;
+	}
+
+	@media only screen and (max-width: 1024px) {
+		.header {
+			display: none;
+		}
+	}
+
+	@media only screen and (min-width: 1024px) {
+		.footer {
+			display: none;
+		}
+	}
+</style>
+
+<main class="page">
+	<header class="header">
+		<a href="/" class="title">Bostoen Toby</a>
+		<nav class="nav">
+			<a href="/" class="navigation-top">Home</a>
+			<a href="/projects" class="navigation-top">Projects</a>
+			<a href="/about" class="navigation-top">About</a>
+			<a
+				href="/cv.pdf"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="navigation-top">
+				CV
+			</a>
+		</nav>
+		<div class="container-right">
+			<button class="sun-container">
+				<Sun class="sun" />
+			</button>
+			<button class="contact">Contact</button>
+		</div>
+	</header>
+	<slot class="container" />
+	<footer class="footer">
+		<nav class="footer-container">
+			<a href="/" class="navigation-bottom">
+				<Home />
+				<p>Home</p>
+			</a>
+			<a href="/projects" class="navigation-bottom">
+				<Cpu />
+				<p>Projects</p>
+			</a>
+			<a href="/about" class="navigation-bottom">
+				<Contact />
+				<p>About</p>
+			</a>
+			<a
+				href="/cv.pdf"
+				target="_blank"
+				rel="noopener norefferer"
+				class="navigation-bottom">
+				<Download />
+				<p>CV</p>
+			</a>
+		</nav>
+	</footer>
+</main>
--- apps/portfolio/src/routes/+page.svelte
+++ apps/portfolio/src/routes/+page.svelte
@@ -1,2 +1,145 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const languages = [
+		'HTML5',
+		'CSS3',
+		'Javascript',
+		'Typescript',
+		'TailwindCSS',
+		'Svelte',
+		'VueJS',
+		'React',
+		'C#',
+		'Python',
+		'MySQL',
+	]
+</script>
+
+<style scoped>
+	.container {
+		display: flex;
+		justify-content: center;
+		gap: 6rem;
+		flex: 1 1 auto;
+		align-items: center;
+	}
+
+	.information {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		font-size: 3rem;
+		color: var(--green);
+		padding-bottom: 2rem;
+	}
+
+	.subtitle {
+		font-size: 2rem;
+	}
+
+	.image {
+		display: block;
+		width: auto;
+		height: auto;
+		max-width: 30vw;
+		border-radius: 15px;
+	}
+
+	.grid-lang {
+		padding-top: 2rem; 
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		gap: 1rem;
+	}
+
+	.container-lang {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
+		background-color: var(--gray);
+		border-radius: 5px;
+	}
+
+	.icon {
+		font-size: 2rem;
+		color: var(--green);
+		padding-bottom: 0.5rem;
+	}
+
+	@media only screen and (max-width: 1024px) {
+		.container {
+			display: flex;
+			flex-direction: column;
+			gap: 2rem;
+		}
+
+		.image {
+			max-width: 50vw;
+			margin-bottom: 2rem;
+		}
+
+		.information {
+			margin-top: 2rem;
+		}
+	}
+
+	@media only screen and (max-width: 640px) {
+		.grid-lang {
+			grid-template-columns: repeat(3, 1fr);
+		}
+
+		.title {
+			font-size: 2rem;
+		}
+
+		.subtitle {
+			font-size: 1rem;
+		}
+
+		.container-lang {
+			padding: 8px;
+		}
+	}
+
+	@media only screen and (max-width: 450px) {
+		.grid-lang {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+</style>
+
+<main class="container">
+	<section class="information">
+		<h1 class="title">Bostoen Toby</h1>
+		<h2 class="subtitle">Full-stack Web Developer</h2>
+		<h3 class="subtitle">based in Belgium</h3>
+		<div class="grid-lang">
+			{#each languages as language}
+				{#if language != 'C#'}
+					<div class="container-lang">
+						<i class="devicon-{language.toLocaleLowerCase()}-plain icon" />
+						<p>{language}</p>
+					</div>
+				{:else}
+					<div class="container-lang">
+						<i class="devicon-csharp-plain icon" />
+						<p>{language}</p>
+					</div>
+				{/if}
+			{/each}
+		</div>
+	</section>
+
+	<img
+		height="2179"
+		width="2136"
+		class="image"
+		src="personalimage.jpg"
+		alt="Bostoen Toby smiling to the camera" />
+
+</main>
